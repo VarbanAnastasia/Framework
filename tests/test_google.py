a662493c1f6ee844a52cc22fa588aa3b32fdaf44
VarbanAnastasia <145008875+VarbanAnastasia@users.noreply.github.com>
2025-05-22 11:04:40 +0300

    обновила тест

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,5 +1,8 @@
 import requests
+import allure
 
+@allure.title("Проверка, что GET-запрос возвращает 200")
+@allure.feature("HTTP-запросы")
 def test_get_status_code():
     """Проверка, что запрос возвращает 200"""
     response = requests.get("https://httpbin.org/get")
