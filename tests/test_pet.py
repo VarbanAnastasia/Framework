fdb3e8a6f22d72563e8b2d59fdd796faebf935bb
VarbanAnastasia <145008875+VarbanAnastasia@users.noreply.github.com>
2025-05-22 17:08:11 +0300

    test 200

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -7,3 +7,4 @@ def test_get_status_code():
     """Проверка, что запрос возвращает 200"""
     response = requests.get("https://petstore.swagger.io/v2/pet/findByStatus")
     assert response.status_code == 200
+
