3da9e96a31adfdc74580ec463966b9c47ae3bb98
VarbanAnastasia <145008875+VarbanAnastasia@users.noreply.github.com>
2025-05-22 16:57:05 +0300

    дженкинс новый fga

--- tests/test_google.py
+++ tests/test_google.py
@@ -6,4 +6,4 @@ import allure
 def test_get_status_code():
     """Проверка, что запрос возвращает 200"""
     response = requests.get("https://httpbin.org/get")
-    assert response.status_code == 200
+    assert response.status_code == 503
